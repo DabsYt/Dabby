da687e450ac6a511c9ab20080a788a9aaaf34bef
DabsYt <44115743+DabsYt@users.noreply.github.com>
2020-04-24 20:11:46 +0300

    Update uptime.py

--- uptime.py
+++ uptime.py
@@ -3,9 +3,10 @@ from discord.ext import commands
 import asyncio
 import os
 
-async def keepalive(message):
+async def keepalive(user,message):
+ while True:
   for i in range(9999999):
-    await message.channel.send("@Dabby[Beta]#3410")
+    await message.channel.send(user.mention)
     await asyncio.sleep(5)
 
 bot=commands.Bot(command_prefix="D")
@@ -14,10 +15,9 @@ bot=commands.Bot(command_prefix="D")
 async def on_ready():
   print("Up")
 
-@bot.event
-async def on_message(message):
-  if(message.content=="botkeepalive"):
-      bot.loop.create_task(keepalive(message))
-      await message.channel.send("Done!")
-      
+@bot.command()
+async def keep(ctx,user:discord.Member):
+  bot.loop.create_task(user,ctx.message)
+  await ctx.send("Done!")
+
 bot.run(os.getenv("Ttoken"))
