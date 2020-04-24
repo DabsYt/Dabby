f8cd6e1f8e8ce85a2f87090e69188655002608df
DabsYt <44115743+DabsYt@users.noreply.github.com>
2020-04-24 20:02:55 +0300

    Uptime for dabby beta self host

---
+++ uptime.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+async def keepalive(message):
+  for i in range(9999999):
+    await message.channel.send("@Dabby[Beta]#3410")
+    await asyncio.sleep(5)
+
+bot=commands.Bot(command_prefix="D")
+
+@bot.event
+async def on_ready():
+  print("Up")
+
+@bot.event
+async def on_message(message):
+  if(message.channel.id==702167627094818987):
+    if(message.content=="botkeepalive"):
+      await bot.loop.create_task(keepalive(message))
+      await message.channel.send("Done!")
+      
+bot.run(os.getenv("Ttoken"))
